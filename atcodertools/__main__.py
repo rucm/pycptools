6f6544b93c37f43fd1529dd1b14be42ee83682ca
cm ru <cm ru>
2018-02-20 22:07:34 +0900

    added __main__.py

---
+++ atcodertools/__main__.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    if __package__ == '':
+        import os.path
+        path = os.path.dirname(os.path.dirname(__file__))
+        sys.path[0:0] = [path]
+    import core
+    sys.exit(core.main())
+
+
+if __name__ == "__main__":
+    sys.exit(main())
