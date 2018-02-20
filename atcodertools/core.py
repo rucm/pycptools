098d29cddacb94a13781edf2ee1eea43c7d03151
rucm <st16423@kagawa.kosen-ac.jp>
2018-02-20 17:49:52 +0900

    added base.

---
+++ atcodertools/core.py
@@ -0,0 +1,24 @@
+import fire
+
+from problem_list import ProblemList
+
+
+class Commands(object):
+
+    def fetch(self, contest_id):
+        problem_list = ProblemList(contest_id)
+        problem_list.fetch()
+        problem_list.save()
+
+    def create(self, contest_id=None, filename=None):
+        if contest_id is not None:
+            problem_list = ProblemList(contest_id)
+            problem_list.fetch()
+            print('problem fetch success')
+        elif filename is not None:
+            problem_list = ProblemList()
+            problem_list.load(filename)
+            print('problem load success')
+
+if __name__ == '__main__':
+    fire.Fire(Commands)
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='atcodertools',
+        version='1.0.0',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'fire',
+            'requests',
+            'lxml',
+            'cssselect'
+        ],
+        entry_points={
+            'console_scripts': [
+                'atcodertools = atcodertools.cli:create'
+            ]
+        }
+    )
