2de81021f97e0a7d96da2e1b275e4907da674018
cm ru <cm ru>
2018-04-25 22:59:18 +0900

    added default config settings.

--- common/const.py
+++ common/const.py
@@ -1,5 +1,6 @@
 import configparser
 import sys
+import os
 
 
 class _const:
@@ -9,16 +10,21 @@ class _const:
     def __setattr__(self, name, value):
         if name in self.__dict__:
             raise self.ConstError('Can\'t rebind const ({})'.format(name))
-
         self.__dict__[name] = value
 
     def setup(self):
-        config = configparser.ConfigParser().read('config.ini', 'utf-8')
+        config = configparser.ConfigParser().read('config.ini')
         for section in config.sections():
             for item in config.items(section):
                 self.__setattr__(item[0], item[1])
 
     def default_setup(self):
-        pass
+        config = configparser.ConfigParser()
+        config['main'] = {
+            'HOME_PATH': os.path.join(os.getenv('HOME'), '.cptools')
+        }
+
+        with open('config.ini', 'w') as file:
+            config.write(file)
 
 sys.modules[__name__] = _const()
